b4efda42171f3fb138ad469e20870735c335e67d
Prayush Kumar <prayush.kumar@ligo.org>
2015-06-23 22:36:43 -0400

    modifications to plotting script

--- emcee_PE/scripts/sample_analysis.py
+++ emcee_PE/scripts/sample_analysis.py
@@ -1,11 +1,11 @@
-#
+#!/usr/bin/env python
 # Tools for analysis of MCMC samples from emcee for spin estimation project
 #
 # MP 04/2015
 import os, sys
 import numpy as np
 import matplotlib as mpl
-#mpl.use('Agg')
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 plt.rcParams.update({'text.usetex' : True})
 
@@ -48,6 +48,7 @@ def Mfun(Mc, eta):
   return Mc*eta**(-3.0/5.0)
 
 def chi_eff(eta, chi1, chi2):
+    #{{{
     q = qfun(eta)
     M = 1
     m1 = m1fun(M,q)
@@ -56,8 +57,10 @@ def chi_eff(eta, chi1, chi2):
     chi_a = (chi1 - chi2)/2.0;
     delta = (m1-m2)/M;
     return chi_s + delta * chi_a
+    #}}}
 
 def chi_PN(eta, chi1, chi2):
+    #{{{
     q = qfun(eta)
     M = 1
     m1 = m1fun(M,q)
@@ -66,13 +69,22 @@ def chi_PN(eta, chi1, chi2):
     chi_a = (chi1 - chi2)/2.0;
     delta = (m1-m2)/M;
     return chi_s * (1.0 - 76.0/113.0 * eta) + delta * chi_a # Ajith's PN reduced spin parameter
+    #}}}
 
 def load_samples(dataDir, SNR, burnin=500):
     """
     Load samples from double-spin emcee run. Throw away samples before burnin and at very low likelihood. Convert to match.
     """
-    chain = np.load(dataDir+'/chain.npy')
-    loglike = np.load(dataDir+'/loglike.npy')
+    #{{{
+    chfile = dataDir+'/chain.npy'
+    llfile = dataDir+'/loglike.npy'
+    if not os.path.exists(chfile) or not os.path.exists(llfile):
+      return {'eta' : None, 'Mc' : None, 'chieff' : None, 'chiPN' : None,
+              'chi1' : None, 'chi2' : None, 'Mtot' : None,
+              'm1' : None, 'm2' : None, 'match' : None}
+
+    chain = np.load(chfile)
+    loglike = np.load(llfile)
     
     samples_ok = chain[:, burnin:, :]
     loglike_ok = loglike[burnin:,:] # it's really log posterior pdf, but can get get back match modulo prior
@@ -102,14 +114,16 @@ def load_samples(dataDir, SNR, burnin=500):
     return {'eta' : etaval, 'Mc' : Mcval, 'chieff' : chieffval, 'chiPN' : chiPNval, 
             'chi1' : chi1val, 'chi2' : chi2val, 'Mtot' : Mval, 
             'm1' : m1val, 'm2' : m2val, 'match' : matchval}
-
+    #}}}
 
 def PercentileInterval(s, pc=95.0):
+    #{{{
     offset = (100.0-pc)/2.0
     lowbound = s > np.percentile(s, offset)
     hibound = s < np.percentile(s, 100.0 - offset)
     mask = np.logical_and(lowbound, hibound)
     return s[mask]
+    #}}}
 
 # hist(PercentileInterval(s1[0], pc=99.0), 100);
 
@@ -123,6 +137,7 @@ def get_bias(basedir = '/home/prayush/projects/nsbh/TidalParameterEstimation/Par
     of the corresponding run. Compute the 90% confidence interval and the median
     value of different parameters fromt the posterior. Return both.
     """
+    #{{{
     test_dir = os.path.join(basedir, simdir)
     m1_inj, m2_inj = pnutils.mchirp_eta_to_mass1_mass2(M_inj * eta_inj**0.6, eta_inj)
     params_inj = {'eta' : eta_inj, 'Mtot' : M_inj, 'Mc' : M_inj * eta_inj**0.6,
@@ -135,6 +150,7 @@ def get_bias(basedir = '/home/prayush/projects/nsbh/TidalParameterEstimation/Par
     bias, CI90, width90 = {}, {}, {}
     for param in ['eta', 'Mc', 'chi1', 'chi2', 'm1', 'm2', 'Mtot']:
       S = match['samples'][param]
+      if S == None: return None, [None, None], None
       param_inj = params_inj[param]
       if 'chi' in param: bias[param] = (np.median(S) - param_inj) / 1.
       else: bias[param] = (np.median(S) - param_inj) / param_inj
@@ -143,6 +159,7 @@ def get_bias(basedir = '/home/prayush/projects/nsbh/TidalParameterEstimation/Par
     
     #print "bias = %f, width90 = %f" % (bias, width90)
     return bias, CI90, width90
+    #}}}
 
 ######################################################
 # Function to make parameter bias plots
@@ -152,6 +169,7 @@ linecolors = ['r', 'g', 'b', 'k', 'm', 'y']
 
 def make_bias_plot(plotparam='Mc', plotquantity='bias', normalize='',\
                     xlabel='$\\rho$', ylabel=None, savefig='plots/plot.png'):
+  #{{{
   # parameter bias
   plt.figure(int(1e7 * np.random.random()))
   for pltid, q in enumerate(qvec):
@@ -173,6 +191,10 @@ def make_bias_plot(plotparam='Mc', plotquantity='bias', normalize='',\
               bias, _, width90 = get_bias(simdir=simstring+simdir+'/',\
                       M_inj=q*mNS, eta_inj=q/(1.+q)**2,\
                       chi1_inj=chi1val, chi2_inj=chi2val)
+              if bias == None:
+                print >> sys.stdout, "Data for %s not found. Ignoring.."\
+                                        % (simstring+simdir)
+                continue
               for kk in bias:
                 if kk in param_bias: param_bias[kk].append(bias[kk])
                 else: param_bias[kk] = [bias[kk]]
@@ -207,6 +229,7 @@ def make_bias_plot(plotparam='Mc', plotquantity='bias', normalize='',\
     if pltid == 0 and ylabel: plt.ylabel(ylabel)
     plt.title('$q = %.1f$' % q)
   plt.savefig(savefig)
+  #}}}
 
 ######################################################
 # Set up parameters of signal
@@ -216,7 +239,9 @@ chi2vec = [-0.5, 0, 0.5]  # larger BH
 mNS = 1.35
 qvec = [2, 3]
 Lambdavec = [500, 1000, 2000]
-SNRvec = [20, 30, 40, 50, 60, 70, 80, 90, 100]
+#SNRvec = [20, 30, 40, 50, 60, 70, 80, 90, 100]
+SNRvec = [20, 30, 50, 70, 90, 120]
+
 
 ######################################################
 # Set up parameters of templates
@@ -227,7 +252,7 @@ m1max = 15.
 m2min = 1.2 
 m2max = 25
 
-LambdaMax = 2000
+LambdaMax = 4000
 Lambdastdev = 100
 
 inject_tidal = True
@@ -257,7 +282,7 @@ else: tmpstring = 'N'
 simstring = sigstring + tmpstring + '_'
 
 Nwalkers = [100]
-Nsamples = [6000]
+Nsamples = [150000]
 Nburnin  = 500
 
 ######################################################
